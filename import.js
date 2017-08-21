c5b8fbe55a9916345316500b2b09914986150f5c
knuxbbs <knuxbbs@gmail.com>
2017-08-20 22:34:09 -0300

    Remove getSchemaForGame call, add 'include_appinfo' parameter in steam.js

--- import.js
+++ import.js
@@ -2,19 +2,24 @@ var s = require("./steam-config");
 var steamCountries = require('./data/steam_countries.min.json');
 var fs = require('fs');
 var steamUserDataArray = [];
-var itemsProcessed = 0;
+var processedItems = 0;
 var counter = 0;
-const max = 1000;
+const max = 10000;
 
 getSteamWebApiData("76561198168752057", repeatCallback);
 
 function repeatCallback(newSteamId) {
-  itemsProcessed = 0;
+  processedItems = 0;
+
   if (newSteamId !== "") {
     getSteamWebApiData(newSteamId, repeatCallback);
   } else {
-    console.log("Importação finalizada.");
-    writeJsonFile();
+    Promise.all(steamUserDataArray).then(function () {
+      console.log("Importação finalizada.");
+      writeJsonFile();
+    }, function (err) {
+      
+    });
   }
 }
 
@@ -32,7 +37,7 @@ function getSteamWebApiData(steamid, callback) {
       if (data1.friendslist) {
         data1.friendslist.friends.forEach(function (friend) {
 
-          itemsProcessed++;
+          processedItems++;
 
           s.getPlayerSummaries({
             steamids: friend.steamid,
@@ -50,10 +55,14 @@ function getSteamWebApiData(steamid, callback) {
                     if (player.locstatecode) {
                       state = country.states[player.locstatecode].name;
 
-                      if (player.loccityid) {
+                      //Algumas cidades não possuem o nome listado em steam_countries.min.json
+                      if (player.loccityid && country.states[player.locstatecode].cities[player.loccityid] &&
+                        country.states[player.locstatecode].cities[player.loccityid].coordinates) {
+
                         city = country.states[player.locstatecode].cities[player.loccityid].name;
                         longitude = country.states[player.locstatecode].cities[player.loccityid].coordinates.split(",")[0];
                         latitude = country.states[player.locstatecode].cities[player.loccityid].coordinates.split(",")[1];
+
                       } else {
                         longitude = country.states[player.locstatecode].coordinates.split(",")[0];
                         latitude = country.states[player.locstatecode].coordinates.split(",")[1];
@@ -66,6 +75,7 @@ function getSteamWebApiData(steamid, callback) {
 
                   s.getOwnedGames({
                     steamid: friend.steamid,
+                    include_appinfo: 1,
                     callback: function (err, data3) {
                       if (data3 && data3.response && data3.response.games) {
                         //Obtém o game com mais tempo de jogo
@@ -74,45 +84,39 @@ function getSteamWebApiData(steamid, callback) {
                         //Obtém o objeto que representa o game
                         var mostPlayedGame = data3.response.games.find(function (o) { return o.playtime_forever == maxPlaytimeForever; })
 
-                        s.getSchemaForGame({
-                          appid: mostPlayedGame.appid,
-                          callback: function (err, data4) {
+                        if (countrycode) {
+                          var localization = countrycode;
 
-                            if (data4 && data4.game && countrycode) {
-                              var localization = countrycode;
-
-                              if (state) {
-                                localization = state + ", " + localization;
-                                if (city) {
-                                  localization = city + ", " + localization;
-                                }
-                              }
-
-                              //Cria objeto a ser inserido no JSON
-                              var steamUserData = getSteamUserData(friend.steamid, username, mostPlayedGame.appid,
-                                data4.game.gameName, localization, longitude, latitude);
-
-                              //Verifica se dados do jogador já foram inseridos ao vetor de elementos antes de adicioná-lo
-                              var elementPos = steamUserDataArray.map(function (x) { return x.steamid; }).indexOf(friend.steamid);
-                              if (!steamUserDataArray[elementPos]) steamUserDataArray.push(steamUserData);
-
-                              console.log(++counter);
-                              console.log("User id: " + friend.steamid);
-                              console.log("User name: " + username);
-                              console.log("Most played game id: " + mostPlayedGame.appid);
-                              console.log("Most played game name: " + data4.game.gameName);
-                              console.log("Localization: " + localization);
-                              console.log("Longitude: " + longitude);
-                              console.log("Latitude: " + latitude);
-                              console.log();
-                            }
-
-                            if (itemsProcessed === data1.friendslist.friends.length) {
-                              newSteamId = getRandomId(data1.friendslist.friends);
-                              callback(newSteamId);
+                          if (state) {
+                            localization = state + ", " + localization;
+                            if (city) {
+                              localization = city + ", " + localization;
                             }
                           }
-                        })
+
+                          //Cria objeto a ser inserido no JSON
+                          var steamUserData = getSteamUserData(friend.steamid, username, mostPlayedGame.appid,
+                            mostPlayedGame.name, mostPlayedGame.img_logo_url, localization, longitude, latitude);
+
+                          //Verifica se dados do jogador já foram inseridos ao vetor de elementos antes de adicioná-lo
+                          var elementPos = steamUserDataArray.map(function (x) { return x.steamid; }).indexOf(friend.steamid);
+                          if (!steamUserDataArray[elementPos]) steamUserDataArray.push(steamUserData);
+
+                          console.log(++counter);
+                          console.log("User id: " + friend.steamid);
+                          console.log("User name: " + username);
+                          console.log("Most played game id: " + mostPlayedGame.appid);
+                          console.log("Most played game name: " + mostPlayedGame.name);
+                          console.log("Localization: " + localization);
+                          console.log("Longitude: " + longitude);
+                          console.log("Latitude: " + latitude);
+                          console.log();
+                        }
+
+                        if (processedItems === data1.friendslist.friends.length) {
+                          newSteamId = getRandomId(data1.friendslist.friends);
+                          callback(newSteamId);
+                        }
                       }
                     }
                   })
@@ -136,9 +140,10 @@ function getSteamUserData() {
     username: arguments[1],
     mostPlayedGameId: arguments[2],
     mostPlayedGameName: arguments[3],
-    localization: arguments[4],
-    longitude: arguments[5],
-    latitude: arguments[6]
+    mostPlayedGameLogo: "http://media.steampowered.com/steamcommunity/public/images/apps/" + arguments[2] + "/" + arguments[4] + ".jpg",
+    localization: arguments[5],
+    longitude: arguments[6],
+    latitude: arguments[7]
   };
 
   return steamUserData;
@@ -148,7 +153,7 @@ function getRandomId(dataArray) {
   //Obtém id aleatório dentre a lista de amigos para que seja feita nova busca.
   var newSteamId = dataArray[Math.floor(Math.random() * dataArray.length)].steamid;
 
-  if (counter <= max) {
+  if (steamUserDataArray.length <= max) {
     return newSteamId;
   } else {
     return "";
